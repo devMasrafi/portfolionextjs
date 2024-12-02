6acb1ba8d7d41bb1a80aae3e785064344c2f515c
Md. Masrafi Mondol <146985678+devMasrafi@users.noreply.github.com>
2024-12-02 11:31:57 +0600

    update on project

--- src/components/project.jsx
+++ src/components/project.jsx
@@ -26,48 +26,61 @@ const ProjectPage = () => {
   ];
 
   return (
-    <main className="bg-darkBlue">
-      <section className=" lg:pt-[15rem] text-white ">
-        <div className="flex">
-          <div>
+    <main className="bg-darkBlue ">
+      <section className=" relative container mx-auto lg:py-[6rem] text-white ">
+        <div className="before:content-[''] lg:before:h-[2px] lg:before:w-full lg:before:absolute lg:before:bg-white lg:before:top-[4rem] lg:before:left-0 lg:before:rounded-full lg:before:opacity-60 ">
+          <div className="flex">
+            {/* project intro info */}
             <div>
-              <h3>my works</h3>
-              <h2>work&#39;s that i&#39;m proud of</h2>
-            </div>
-            <button>see all works</button>
-          </div>
-          {/* Project Hero */}
-          <div className="lg:w-[55rem]">
-            <img
-              className="lg:w-full "
-              src={proiject1[0].image}
-              alt="project 1"
-            />
-            <div className="flex justify-between ">
-              <div className="lg:w-[34rem] ">
-                <h2 className="lg:text-7xl capitalize">
-                  {proiject1[0].titile}
-                </h2>
-                <p>{proiject1[0].details}</p>
-              </div>
-              <div className=" flex flex-wrap gap-x-2 gap-y-1 lg:w-[16rem] ">
-                {proiject1[0].techused.map((items) => {
-                  return (
-                    <div
-                      className="border border-1 border-white lg:px-4 lg:py-2 rounded-md"
-                      key={items.techId}
-                    >
-                      <p>{items.tech}</p>
-                    </div>
-                  );
-                })}
+              <div className="flex gap-x-5 ">
+                <h3 className="lg:text-2xl capitalize font-ralewayFt text-nowrap opacity-80 ">
+                  my works
+                </h3>
+                <div>
+                  <h2 className="font-ralewayFt capitalize font-semibold lg:text-5xl text-wrap ">
+                    work&#39;s that i&#39;m proud of
+                  </h2>
+                  <button className="font-robotoFt font-light lg:mt-[2rem] lg:px-6 lg:py-2 border border-1 rounded-xl capitalize tracking-wide  ">
+                    see all works
+                  </button>
+                </div>
               </div>
             </div>
+            {/* Project Hero */}
+            <div className="lg:w-[55rem]">
+              <img
+                className="lg:w-[54rem] lg:h-[28rem] object-cover overflow-hidden bg-gray-500 lg:p-1 lg:rounded-3xl  "
+                src={proiject1[0].image}
+                alt="project 1"
+              />
+              <div className="flex justify-between lg:mt-[2rem] ">
+                <div className="lg:w-[34rem] ">
+                  <h2 className="lg:text-7xl capitalize font-ralewayFt font-semibold">
+                    {proiject1[0].titile}
+                  </h2>
+                  <p className="font-ralewayFt tracking-wider opacity-70 lg:pt-[1rem] lg:w-[30rem] ">{proiject1[0].details}</p>
+                </div>
+                <div className=" flex flex-wrap lg:gap-x-2 lg:gap-y-2 justify-end lg:w-[16rem] ">
+                  {proiject1[0].techused.map((items) => {
+                    return (
+                      <div
+                        className="border border-1 border-white lg:px-4 lg:py-2 lg:rounded-md"
+                        key={items.techId}
+                      >
+                        <p className="capitalize tracking-wider font-ralewayFt lg:text-sm">{items.tech}</p>
+                      </div>
+                    );
+                  })}
+                </div>
+              </div>
+            </div>
           </div>
-        </div>
 
-        {/* 2nd projects */}
-        <section></section>
+          {/* 2nd projects */}
+          <section>
+            
+          </section>
+        </div>
       </section>
     </main>
   );
